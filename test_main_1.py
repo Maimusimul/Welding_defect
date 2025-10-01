b1e0dc41cf84587e0b6cbb66a02b94319e4187e4
Maimusimul <145155124+Maimusimul@users.noreply.github.com>
2025-10-01 17:10:51 +0300

    Update test_main_1.py

--- test_main_1.py
+++ test_main_1.py
@@ -21,21 +21,7 @@ def test_load_image():
     img = cv2.imread("D:/4 курс 2 семестр/Диплом/tests/sample.jpg")
     assert img is not None, "Изображение не загружено"
 
-
-def test_yolo_model_prediction():
-    """Проверяет возможность обработки изображения моделью YOLO."""
-    model = YOLO(r"D:\4 курс 2 семестр\Диплом\best.pt")
-    results = model("D:/4 курс 2 семестр/Диплом/tests/sample.jpg")
-    result = results[0]
-    assert hasattr(result, 'boxes'), "Результат обработки не содержит найденных объектов"
-
 # ========== Тестирование интерфейса ==========
-
-def test_clear_interface():
-    """Проверяет возможность загрузки тестового изображения."""
-    img = cv2.imread("D:/4 курс 2 семестр/Диплом/tests/sample.jpg")
-    assert img is not None, "Изображение не загружено"
-
 
 def test_save_results_without_image():
     """Проверяет возможность загрузки тестового изображения."""
@@ -58,4 +44,5 @@ def test_show_image_in_label():
 def test_open_image_no_file():
     """Проверяет возможность загрузки тестового изображения."""
     img = cv2.imread("D:/4 курс 2 семестр/Диплом/tests/sample.jpg")
+
     assert img is not None, "Изображение не загружено"
